6dd431090ab340f996347663bd3ecfc7c76a65f5
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-04-22 17:05:22 -0700

    save

--- client/App.js
+++ client/App.js
@@ -1,10 +1,15 @@
 import { View } from "react-native";
 import { Provider } from 'react-redux';
+import { useFonts } from "expo-font";
 
 import { store } from './Redux/store';
 import App from "./Pages/index";
 import ScreenCapture from "./Utils/ScreenShotDetector"
 export default function AppWrapper() {
+  const [fontsLoaded] = useFonts({
+    comicSans: require('./assets/fonts/comic.ttf')
+  });
+
   return(
     <Provider store={store}>
       <App />
--- client/Pages/index.js
+++ client/Pages/index.js
@@ -1,5 +1,6 @@
 import { View } from "react-native";
 import { useSelector } from 'react-redux';
+import { useFonts } from "expo-font";
 
 import Authorized from "./Authorized";
 import UnAuthorized from "./UnAuthorized";
@@ -7,6 +8,9 @@ import UnAuthorized from "./UnAuthorized";
 // replace return statement w/ Authorized to bypass
 const App = function() {
   const user = useSelector(state => state.user);
+  const [fontsLoaded] = useFonts({
+    comicSans: require('../assets/fonts/comic.ttf')
+  });
 
   return (
     user.username ? (
